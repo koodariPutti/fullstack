4ea7186edba4692507d736dc9404e0dc6dee3729
lindee <eetu.p.lindfors@student.jyu.fi>
2025-09-17 10:10:40 +0300

    osa3 tehtäviä ja deploy to render

---
+++ osa3/index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+const morgan = require('morgan')
+app.use(morgan('tiny'))
+
+morgan.token('body', (request) => JSON.stringify(request.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
+
+let persons = [
+  {
+    "id": "1",
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": "2",
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": "3",
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": "4",
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const count = persons.length
+  const time = new Date()
+  response.send(`<p>Phonebook has info for ${count} people</p><p>${time}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = String(request.params.id)
+  const person = persons.find(p => p.id === id)
+  if (!person) return response.status(404).end()
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'name or number missing' })
+  }
+
+  if (persons.some(person => person.name === body.name)) {
+    return response.status(400).json({ error: 'name must be unique' })
+  }
+
+  const newPerson = {
+    id: String(Math.floor(Math.random() * 1000000000)),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(newPerson)
+  response.status(201).json(newPerson)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+
