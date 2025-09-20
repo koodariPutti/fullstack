e2c06fb4586632ff875047b6781f228b0bd6cdaf
lindee <eetu.p.lindfors@student.jyu.fi>
2025-09-20 12:28:40 +0300

    teht 3.12 ja osa 3.13

--- osa3/index.js
+++ osa3/index.js
@@ -37,7 +37,9 @@ let persons = [
 ]
 
 app.get('/api/persons', (request, response) => {
+    Person.find({}).then(persons => {
     response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
@@ -80,6 +82,27 @@ app.post('/api/persons', (request, response) => {
   response.status(201).json(newPerson)
 })
 
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.yfthjvw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String, 
+    number: String 
+})
+const Person = mongoose.model('Person', personSchema) 
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
---
+++ osa3/mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.yfthjvw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String, 
+    number: String 
+})
+const Person = mongoose.model('Person', personSchema)
+
+if (!name || !number) {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(p => console.log(`${p.name} ${p.number}`))
+    mongoose.connection.close()
+  })
+} else {
+  const person = new Person({ name, number })
+  person.save().then(p => {
+    console.log(`added ${p.name} number ${p.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
