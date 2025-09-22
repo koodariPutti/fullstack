886867fdd97ec73e1c10f621e81ba06791a50697
lindee <eetu.p.lindfors@student.jyu.fi>
2025-09-22 15:48:41 +0300

    4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogs) => {
+  const likes = blogs.map(blog => blog.likes || 0);
+  const total = likes.reduce((sum, current) => sum + current, 0);
+
+  return total;
+};
+
 module.exports = {
-  dummy
-}
+  dummy,
+  totalLikes,
+};
